7b9c906c2b5c76f4354360c9e05537218b816491
deepaktalwardt <deepaktalwardt@gmail.com>
2019-09-29 12:54:01 -0700

    Added better solution for min-rewards problem

--- AlgoExpert/min-rewards.cpp
+++ AlgoExpert/min-rewards.cpp
@@ -1,10 +1,59 @@
+////////////////////////////////////////////////////////
+// Time Complexity: O(N), Space Complexity: O(N)
+
+#include <vector>
+#include <numeric>
+#include <iostream>
+
+using namespace std;
+
+vector<int> findValleys(vector<int>& scores) {
+	if (scores.size() == 1) return {0};
+	vector<int> result;
+	for (int i = 0; i < scores.size(); i++) {
+		if (i == 0 && scores[i] < scores[i + 1]) {
+				result.push_back(i);
+			}
+		if (i == scores.size() - 1 && scores[i] < scores[i - 1]) {
+			result.push_back(i);
+		}
+		if (i == 0 || i == scores.size() - 1) continue;
+		if (scores[i] < scores[i-1] && scores[i] < scores[i+1]) {
+			result.push_back(i);
+		}
+	}
+	return result;
+}
+
+int minRewards(vector<int> scores) {
+  // Write your code here.
+	vector<int> valleys = findValleys(scores);
+	vector<int> rewards(scores.size(), 1);
+	for (int v : valleys) {
+		int left = v - 1;
+		int right = v + 1;
+		while (left >= 0 && scores[left] > scores[left + 1]) {
+			rewards[left] = max(rewards[left], rewards[left + 1] + 1);
+			left--;
+		}
+		while (right < scores.size() && scores[right] > scores[right - 1]) {
+			rewards[right] = max(rewards[right], rewards[right - 1] + 1);
+			right++;
+		}
+	}
+	return accumulate(rewards.begin(), rewards.end(), 0);
+}
+
+////////////////////////////////////////////////////////
+// Time Complexity: O(N^2), Space Complexity: O(N)
+
 #include <vector>
 #include <algorithm>
 #include <numeric>
 
 using namespace std;
 
-// Time Complexity: O(N^2), Space Complexity: O(N)
+
 int minRewards(vector<int> scores) {
   // Write your code here.
 	vector<int> rewards(scores.size(), 1);
