a7bb1dc6c2f51b86a7e0c6dc4e8c5f1299d4b2ff
deepaktalwardt <deepaktalwardt@gmail.com>
2019-10-20 16:44:54 -0700

    Added problem 2 for day 36

--- LeetCode/221-maximal-square.cpp
+++ LeetCode/221-maximal-square.cpp
@@ -1,3 +1,24 @@
+// Dynamic Programming: Time O(mn), Space: O(mn)
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        if (rows == 0) return 0;
+        int cols = matrix[0].size();
+        vector<vector<int>> maxSquareCorner(rows + 1, vector<int>(cols + 1, 0));
+        int result = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    maxSquareCorner[i][j] = min(maxSquareCorner[i - 1][j - 1], min(maxSquareCorner[i - 1][j], maxSquareCorner[i][j - 1])) + 1;
+                    result = max(result, maxSquareCorner[i][j]);
+                }
+            }
+        }
+        return result * result;
+    }
+};
+
 // Brute force Time: O((mn)^2), Space: O(1)
 class Solution {
 public:
