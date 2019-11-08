ddc854a4ec9e07d2a1f48f406e37f9313a8fcec6
Deepak Talwar <deepaktalwardt>
2019-11-07 16:32:32 -0800

    Added problem 1 for day 54

---
+++ LeetCode/841-keys-and-rooms.cpp
@@ -0,0 +1,37 @@
+
+// Iterative DFS
+#include <stack>
+
+class Solution {
+public:
+    void openRooms(vector<vector<int>>& rooms, int i, vector<bool>& opened) {
+        stack<int> s;
+        s.push(i);
+        while (!s.empty()) {
+            int currRoom = s.top();
+            s.pop();
+            for (auto& n : rooms[currRoom]) {
+                if (!opened[n]) {
+                    s.push(n);
+                    opened[n] = true;
+                }
+            }
+        }
+    }
+    
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> opened(n, false);
+        opened[0] = true;
+        for (int i = 0; i < n; i++) {
+            if (opened[i]) {
+                openRooms(rooms, i, opened);
+            }
+        }
+        for (bool val : opened) {
+            if (!val) return false;
+        }
+        return true;
+    }
+};
