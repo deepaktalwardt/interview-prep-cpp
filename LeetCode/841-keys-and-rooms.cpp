6797c8dadacd868008a376f2e53793d88b7ff9ba
Deepak Talwar <deepaktalwardt>
2019-11-07 16:49:08 -0800

    Added problem 2 for day 54

--- LeetCode/841-keys-and-rooms.cpp
+++ LeetCode/841-keys-and-rooms.cpp
@@ -1,3 +1,27 @@
+// Recursive DFS
+class Solution {
+public:
+    
+    void openRooms(vector<vector<int>>& rooms, int i, vector<bool>& opened) {
+        if (opened[i]) return;
+        opened[i] = true;
+        for (int& n : rooms[i]) {
+            openRooms(rooms, n, opened);
+        }
+    }
+    
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> opened(n, false);
+        openRooms(rooms, 0, opened);
+        for (bool val : opened) {
+            if (!val) return false;
+        }
+        return true;
+    }
+};
+
 
 // Iterative DFS
 #include <stack>
