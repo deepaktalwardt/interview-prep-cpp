cf20a44d179997378d8ac112123575b87267ddb1
deepaktalwardt <deepaktalwardt@gmail.com>
2019-12-03 09:44:48 -0800

    Added Problem 1 for day 59

--- AlgoExpert/permutations.cpp
+++ AlgoExpert/permutations.cpp
@@ -1,4 +1,36 @@
+// Solution 2 (Better)
+#include <vector>
+using namespace std;
 
+void swap(vector<int>& array, int i, int j) {
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+void helper(int i, vector<int>& array, vector<vector<int>>& perms) {
+	if (i == array.size() - 1) {
+		vector<int> onePerm = array;
+		perms.push_back(onePerm);
+	} else {
+		for (int j = i; j < array.size(); j++) {
+			swap(array, i, j);
+			helper(i + 1, array, perms);
+			swap(array, i, j);
+		}
+	}
+}
+
+vector<vector<int>> getPermutations(vector<int> array) {
+  // Write your code here.
+	if (array.size() == 0) return {};
+	vector<vector<int>> result;
+	helper(0, array, result);
+	return result;
+}
+
+
+// Solution 1 (Worse)
 // Time: O(N^2 * N!), Space: O(N * N!)
 #include <vector>
 using namespace std;
