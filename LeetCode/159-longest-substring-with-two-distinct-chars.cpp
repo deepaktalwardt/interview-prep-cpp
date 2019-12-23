cff3445297f3ed016fa0475d5209f4343979869d
Deepak Talwar <deepaktalwardt@gmail.com>
2019-12-23 10:05:40 -0800

    Updated solution to LC 159

--- LeetCode/159-longest-substring-with-two-distinct-chars.cpp
+++ LeetCode/159-longest-substring-with-two-distinct-chars.cpp
@@ -1,3 +1,34 @@
+// Faster Solution as we don't clear the hashmap each time
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        unordered_map<char, int> uniqueChars;
+        int longest = 0;
+        int N = s.size();
+        if (N <= 2) return N;
+        int left = 0;
+        int right = 0;
+        while (right < N) {
+            if (uniqueChars.size() <= 2) {
+                uniqueChars[s[right]] = right;
+            }
+            if (uniqueChars.size() == 3) {
+                auto it = min_element(uniqueChars.begin(), uniqueChars.end(), 
+                                     [](const auto& l, const auto& r) {
+                                         return l.second < r.second;
+                                     });
+                int leftmost = it->second;
+                uniqueChars.erase(s[leftmost]);
+                left = leftmost + 1;
+            }
+            longest = max(longest, right - left + 1);
+            right++;
+        }
+        return longest;
+    }
+};
+
+// Slower solution becasue set gets cleared each time
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
